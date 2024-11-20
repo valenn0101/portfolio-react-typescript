04ae27e3ac14183dbeedf3b7f9dcc0047b8b7d3c
valenn0101 <veroncaceresvalentin@gmail.com>
2024-11-20 03:50:23 +0000

    feat(Info): Change data

--- src/components/About/Info.tsx
+++ src/components/About/Info.tsx
@@ -6,21 +6,21 @@ const Info = () => {
       <div className="about__box">
         <i className="bx bx-award about__icon"></i>{" "}
         <h3 className="about__title">Experiencia</h3>
-        <span className="about__subtitle">Junior</span>
+        <span className="about__subtitle">Junior Advance</span>
       </div>
 
       <div className="about__box">
         <i className="bx bx-briefcase-alt about__icon"></i>
         <h3 className="about__title">Practica</h3>
         <span className="about__subtitle">
-          Proyectos, challenges e investigaciones
+          Proyectos profesionales y Universidad
         </span>
       </div>
 
       <div className="about__box">
         <i className="bx bx-message-rounded-dots about__icon"></i>
         <h3 className="about__title">Disponible</h3>
-        <span className="about__subtitle">Online 24/7 </span>
+        <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
   );
